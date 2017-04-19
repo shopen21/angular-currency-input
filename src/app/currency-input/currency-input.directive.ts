1ee571ee4acd00ca7a91acd0dbe3514343dc9d17
a.shafran <shopen21@gmail.com>
2017-04-19 15:43:08 +0300

    wip;

--- src/app/currency-input/currency-input.directive.ts
+++ src/app/currency-input/currency-input.directive.ts
@@ -1,6 +1,6 @@
-import {Directive, ElementRef, forwardRef, HostListener, Input, OnInit, Provider} from "@angular/core";
+import {Directive, ElementRef, forwardRef, HostListener, Input, Provider, Renderer} from "@angular/core";
 import {CurrencyFormatOptions} from "app/currency-input/currency-format-options";
-import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {ControlValueAccessor, DefaultValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 import {CurrencyInputPipe} from "app/currency-input/currency-input.pipe";
 
 const CUSTOM_VALUE_ACCESSOR: Provider = {
@@ -10,33 +10,53 @@ const CUSTOM_VALUE_ACCESSOR: Provider = {
 };
 
 @Directive({
-  selector: 'input[currency-input][type="text"]',
+  selector: 'input[sho-currency-input][type="text"]',
   providers: [CUSTOM_VALUE_ACCESSOR]
 })
 export class CurrencyInputDirective implements ControlValueAccessor {
 
   @Input('currencyInput') inputOptions: CurrencyFormatOptions = new CurrencyFormatOptions();
 
+  @HostListener('input', ['$event.target.value']) onInput(userInputValue): void {
+    let parsedNumber = this.transformer.parse(userInputValue, this.inputOptions);
+    if (Number.isNaN(parsedNumber)) {
+      this.doWriteValue(this.numericValue);
+    }
+    else {
+      this.onChange(parsedNumber);
+    }
+  }
+
   @HostListener('focus') onFocus(): void {
     this.isFocused = true;
+    this.doWriteValue(this.numericValue);
   }
 
   @HostListener('blur') onBlur(): void {
     this.isFocused = false;
+    this.doWriteValue(this.numericValue);
   }
-
-  private onTouched = _ => {
-  };
-  private onChange = _ => {
-  };
 
   private isFocused = false;
 
-  constructor(private element: ElementRef, private transformer: CurrencyInputPipe) {
+  private numericValue: number = Number.NaN;
+
+  private onChange = (_:number) => {
+  };
+
+  constructor(private element: ElementRef,
+              private transformer: CurrencyInputPipe,
+              private valueAccessor: DefaultValueAccessor) {
   }
 
   writeValue(numericValue: number): void {
-    this.element.nativeElement.value = (this.isFocused ? this.transformer.toNumericString : this.transformer.transform)(numericValue, this.inputOptions);
+    this.numericValue = numericValue;
+    this.doWriteValue(numericValue);
+  }
+
+  private doWriteValue(numericValue: number): void {
+    let stringValue = (this.isFocused ? CurrencyInputPipe.toNumericString : this.transformer.transform)(numericValue, this.inputOptions);
+    this.valueAccessor.writeValue(stringValue);
   }
 
   registerOnChange(fn: any): void {
@@ -44,10 +64,10 @@ export class CurrencyInputDirective implements ControlValueAccessor {
   }
 
   registerOnTouched(fn: any): void {
-    this.onTouched = fn;
+    this.valueAccessor.registerOnTouched(fn);
   }
 
   setDisabledState(isDisabled: boolean): void {
-    throw new Error('Method not implemented.');
+    this.valueAccessor.setDisabledState(isDisabled);
   }
 }
--- src/app/currency-input/currency-input.pipe.ts
+++ src/app/currency-input/currency-input.pipe.ts
@@ -2,18 +2,56 @@ import {Pipe, PipeTransform} from "@angular/core";
 import {CurrencyFormatOptions} from "app/currency-input/currency-format-options";
 
 @Pipe({
-  name: 'currencyInput'
+  name: 'currencyAdvanced'
 })
 export class CurrencyInputPipe implements PipeTransform {
-  transform(value: number, options: CurrencyFormatOptions): string {
-    return `${options.prefix || ''}255${options.decimalSeparator}33${options.suffix || ''}`;
+  transform(value: number, options: CurrencyFormatOptions = new CurrencyFormatOptions()): string {
+    let normalizedValue = value ? value : 0;
+    let fraction = normalizedValue % 1;
+    let integer = Math.floor(normalizedValue);
+
+
+    let pieces = {
+      prefix: options.prefix,
+      integer: CurrencyInputPipe.integerToThousands(integer, options.thousandSeparator),
+      separator: options.decimalSeparator,
+      decimal: CurrencyInputPipe.fractionToString(fraction, options.decimalLength),
+      suffix: options.suffix
+    };
+    return `${pieces.prefix}${pieces.integer}${pieces.separator}${pieces.decimal}${pieces.suffix}`;
   }
 
-  parse(value: string, options: CurrencyFormatOptions): number {
-    return 255.33;
+  parse(value: string, options: CurrencyFormatOptions = new CurrencyFormatOptions()): number {
+    if (!value) return 0;
+    value = value.slice(options.prefix.length);
+    if (options.suffix.length) {
+      value = value.slice(0, -options.suffix.length);
+    }
+    let [ integerPart, fractionPart = '' ] = (value || '').split(options.decimalSeparator);
+    integerPart = integerPart.replace(new RegExp(`\\${options.thousandSeparator}`), '');
+
+    return parseFloat(`${integerPart}.${fractionPart}`);
   }
 
-  toNumericString(value: number, options: CurrencyFormatOptions): string {
+  static toNumericString(value: number, options: CurrencyFormatOptions): string {
     return value.toFixed(options.decimalLength);
+  }
+
+  private static integerToThousands(value: number, separator: string): string {
+    let str = value.toFixed(0);
+    let resultArr = [];
+    while (str.length > 0) {
+      resultArr.push(str.slice(-3));
+      str = str.slice(0, -3);
+    }
+
+    return resultArr.join(separator);
+  }
+
+  private static fractionToString(value: number, decimalLength: number): string {
+    decimalLength = Math.floor(decimalLength);
+    decimalLength = decimalLength >= 0 ? decimalLength : 0;
+    let multiplier = Math.pow(10, decimalLength);
+    return Math.floor(value * multiplier).toFixed(0);
   }
 }
